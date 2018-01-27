6c7c63da2378fb2f61bdf64ced7069ede50aeaa6
Oleg Savchuk <osalabs+github@gmail.com>
2018-01-27 22:29:34 +0200

    cron.php sample file

---
+++ www/php/cron.php
@@ -0,0 +1,18 @@
+<?php
+/*
+ Sample crontab script
+ Launch every: X minutes
+ Description: do something regularly offline
+
+ Part of PHP osa framework  www.osalabs.com/osafw/php
+ (c) 2009-2018 Oleg Savchuk www.osalabs.com
+*/
+
+require_once dirname(__FILE__)."/fw/fw.php";
+
+$fw = fw::i(); #get fw instance
+if (!$fw->isOffline()) exit; #prevent run from web browser url
+
+fw::model('Notifications')->sendCron(); #sample model->method call
+
+?>
