62fa563c7e7515726e9e798e62bf8e699c45c222
Oleg Savchuk <osalabs+github@gmail.com>
2018-01-27 21:44:23 +0200

    modern layout for login page

--- www/template/login/index/onload.js
+++ www/template/login/index/onload.js
@@ -1,4 +1,28 @@
+$(document).on('click', '.on-pwd-hideshow', pwd_hideshow);
 pwd_hideshow();
 
-$('#login').focus();
+//$('#login').focus();
 
+$(document).on('keyup change', '.form-label-group input', input_on_change);
+$('.form-label-group input').trigger('change');
+
+
+function input_on_change(e) {
+  var $this=$(this);
+  if ($this.val()>''){
+      if (!$this.is('.filled')) $this.addClass('filled');
+  }else{
+      $this.removeClass('filled');
+  }
+}
+
+function pwd_hideshow(){
+  var chpwd;
+  if ( $('#chpwd')[0].checked ){
+    $('#pwdh').hide();
+    $('#pwd').show().val( $('#pwdh').val() ).trigger('change');
+  }else{
+    $('#pwd').hide();
+    $('#pwdh').show().val( $('#pwd').val() ).trigger('change');
+  }
+}
